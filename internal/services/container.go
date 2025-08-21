73cf9b498beb3452e4c766b1639eb553848695d5
Rakha Dzaky <rakhadzaky9@gmail.com>
2025-08-21 23:48:36 +0700

    feat(custom_variable): add get api custom variable

--- internal/services/container.go
+++ internal/services/container.go
@@ -11,6 +11,7 @@ type ServiceContainer interface {
 	EndpointServiceInterface
 	ScenarioServiceInterface
 	EndpointScenarioServiceInterface
+	CustomVariableServiceInterface
 	MockServiceInterface
 	ToolsService
 }
